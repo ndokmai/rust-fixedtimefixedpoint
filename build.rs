2404ecc2f465b68498bef05a27c0898f017d1d2e
Natnatee Dokmai <ndokmai@indiana.edu>
2020-05-04 23:37:04 -0400

    Make default feature non-SGX

--- build.rs
+++ build.rs
@@ -8,34 +8,55 @@ fn main() {
     let curr_dir_str = env::var_os("CARGO_MANIFEST_DIR").unwrap();
     let curr_dir = Path::new(&curr_dir_str);
     let lib_dir = curr_dir.join(Path::new(LIB));
+    let header_file = lib_dir.join(Path::new("ftfp.h"));
 
     Command::new("git").arg("submodule").arg("update").arg("--init")
         .current_dir(&curr_dir)
         .status()
         .unwrap();
     
-    Command::new("make")
-        .args(&["-C", lib_dir.to_str().unwrap()])
-        .arg("SGX=1")
-        .status()
-        .unwrap();
+    println!("cargo:rustc-link-search={}", lib_dir.to_str().unwrap());
 
-    println!("cargo:rustc-link-lib=static={}", "ftfp_sgx");
-    println!("cargo:rustc-link-search=native={}", lib_dir.to_str().unwrap());
+    let bindings = if cfg!(feature = "sgx") {
+        println!("cargo:rustc-link-lib=static={}", "ftfp_sgx");
+        Command::new("make")
+            .args(&["-C", lib_dir.to_str().unwrap()])
+            .arg("SGX=1")
+            .status()
+            .unwrap();
+        bindgen::Builder::default()
+            .header(header_file.to_str().unwrap())
+            .clang_arg("-DSGX")
+            .whitelist_function("fix_.*")
+            .whitelist_var("FIX_.*")
+            .blacklist_item("FIX_INTERN.*")
+            .blacklist_item("FIX_PRINT.*")
+            .rustfmt_bindings(true)
+            .raw_line("#![allow(non_camel_case_types)]")
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+            .generate()
+            .expect("Unable to generate bindings")
+    } else {
+        println!("cargo:rustc-link-lib=dylib={}", "ftfp");
+        println!("cargo:rustc-env=LD_LIBRARY_PATH={}", lib_dir.to_str().unwrap());
+        Command::new("make")
+            .args(&["-C", lib_dir.to_str().unwrap()])
+            .status()
+            .unwrap();
+        bindgen::Builder::default()
+            .header(header_file.to_str().unwrap())
+            .whitelist_function("fix_.*")
+            .whitelist_var("FIX_.*")
+            .blacklist_item("fix_.*print.*")
+            .blacklist_item("FIX_INTERN.*")
+            .blacklist_item("FIX_PRINT.*")
+            .rustfmt_bindings(true)
+            .raw_line("#![allow(non_camel_case_types)]")
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+            .generate()
+            .expect("Unable to generate bindings")
+    };
 
-    let header_file = lib_dir.join(Path::new("ftfp.h"));
-    let bindings = bindgen::Builder::default()
-        .header(header_file.to_str().unwrap())
-        .clang_arg("-DSGX")
-        .whitelist_function("fix_.*")
-        .whitelist_var("FIX_.*")
-        .blacklist_item("FIX_INTERN.*")
-        .blacklist_item("FIX_PRINT.*")
-        .rustfmt_bindings(true)
-        .raw_line("#![allow(non_camel_case_types)]")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .expect("Unable to generate bindings");
     bindings
         .write_to_file("src/binding.rs")
         .expect("Couldn't write bindings!");
