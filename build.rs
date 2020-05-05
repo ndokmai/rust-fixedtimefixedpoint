ab9791ca7bc98eb89f670d8efbdd8d9ec16591a3
Natnatee Dokmai <ndokmai@indiana.edu>
2020-05-04 22:45:54 -0400

    Fix build.rs

--- build.rs
+++ build.rs
@@ -1,17 +1,31 @@
 use std::process::Command;
+use std::path::Path;
+use std::env;
 
 const LIB: &str = "deps/libfixedtimefixedpoint";
 
 fn main() {
-    Command::new("make")
-        .args(&["-C", LIB, "SGX=1"])
+    let curr_dir_str = env::var_os("CARGO_MANIFEST_DIR").unwrap();
+    let curr_dir = Path::new(&curr_dir_str);
+    let lib_dir = curr_dir.join(Path::new(LIB));
+
+    Command::new("git").arg("submodule").arg("update").arg("--init")
+        .current_dir(&curr_dir)
         .status()
         .unwrap();
+    
+    Command::new("make")
+        .args(&["-C", lib_dir.to_str().unwrap()])
+        .arg("SGX=1")
+        .status()
+        .unwrap();
+
     println!("cargo:rustc-link-lib=static={}", "ftfp_sgx");
-    println!("cargo:rustc-link-search=native={}", LIB);
-    let header_file = format!("{}/ftfp.h", LIB);
+    println!("cargo:rustc-link-search=native={}", lib_dir.to_str().unwrap());
+
+    let header_file = lib_dir.join(Path::new("ftfp.h"));
     let bindings = bindgen::Builder::default()
-        .header(header_file)
+        .header(header_file.to_str().unwrap())
         .clang_arg("-DSGX")
         .whitelist_function("fix_.*")
         .whitelist_var("FIX_.*")
